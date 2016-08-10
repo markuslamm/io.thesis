cb2fe07be6ac877be9954e556a506b1da321eb1d
Markus Lamm <markus.lamm@googlemail.com>
2016-08-10 16:47:54 +0200

    Context load IT for collector-manager

---
+++ collector-manager/collector-manager-app/src/test/java/io/thesis/collector/manager/CollectorManagerAppIT.java
@@ -0,0 +1,15 @@
+package io.thesis.collector.manager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = CollectorManagerApp.class)
+public class CollectorManagerAppIT {
+    
+    @Test
+    public void testContextLoads() {
+    }
+}
