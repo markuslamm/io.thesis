dd21fbf50382150f681c0a48f16759c3f0bea6ac
Markus Lamm <markus.lamm@googlemail.com>
2016-08-10 16:52:27 +0200

    Adjust Docker build script -DskipIT

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -5,7 +5,7 @@ docker rm consul
 docker rm collector-manager
 
 echo "building sources..."
-mvn clean install -Dmaven.test.skip=true -DskipTests
+mvn install -DskipITs
 
 echo "building collector-manager-app..."
 docker build -t io.thesis/collector-manager collector-manager
