4c910a02450002cb72d9c10ae48e829f6ab403cf
Markus Lamm <markus.lamm@googlemail.com>
2016-08-12 17:54:48 +0200

    Added Docker infrastructure for Apache Kafka, boot witch docker-compose

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -12,6 +12,15 @@ mvn install -DskipITs
 echo "building collector-manager-app..."
 docker build -t io.thesis/collector-manager collector-manager
 
+echo "building docker-base..."
+docker build -t wurstmeister/base infrastructure/docker-base
+
+echo "building docker-zookeeper..."
+docker build -t wurstmeister/zookeeper infrastructure/docker-zookeeper
+
+echo "building docker-kafka..."
+docker build -t wurstmeister/kafka infrastructure/docker-kafka
+
 echo "building docker-flink..."
 cd infrastructure/docker-flink
 ./build.sh
