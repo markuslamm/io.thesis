918e90ff2cf6a1642808614d3fcca455f7fe4045
Markus Lamm <markus.lamm@googlemail.com>
2016-08-12 21:09:00 +0200

    Add Docker infrastructure for ELK

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
 echo "cleanup containers..."
-docker rm consul
-docker rm collector-manager
-docker rm flink-jobmanager
-docker rm flink-taskmanager
+docker rm consul collector-manager flink-jobmanager flink-taskmanager kafka elk 1>/dev/null 2>&1
 
 echo "building sources..."
 mvn install -DskipITs
@@ -20,6 +17,9 @@ docker build -t wurstmeister/zookeeper infrastructure/docker-zookeeper
 
 echo "building docker-kafka..."
 docker build -t wurstmeister/kafka infrastructure/docker-kafka
+
+echo "building docker-elk..."
+docker build -t sebp/elk infrastructure/docker-elk
 
 echo "building docker-flink..."
 cd infrastructure/docker-flink
