bb48ede88cb30f5b84316e2877c58e4f0603e5cc
Markus Lamm <markus.lamm@googlemail.com>
2016-08-14 14:53:56 +0200

    Downgrade Flink processor job to 1.7 because of lambda problems

--- collector-data-processor/src/main/java/io/thesis/collector/processor/CollectorDataProcessor.java
+++ collector-data-processor/src/main/java/io/thesis/collector/processor/CollectorDataProcessor.java
@@ -1,19 +1,26 @@
 package io.thesis.collector.processor;
 
 import io.thesis.commons.json.JsonUtils;
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer09;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer09;
 import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
+import org.apache.flink.util.Collector;
 
-import java.util.*;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 
 /**
  * This is just av very simple Flink job, which receives all messages from collector-clients,
  * filters messages of collector type 'flink_jmx', extracts containing JVM data and writes the
  * flattened map into Elasticsearch.
- *
+ * <p>
  * TODO: data handling is a mess
  */
 public class CollectorDataProcessor {
@@ -30,37 +37,78 @@ public class CollectorDataProcessor {
 
         final DataStream<CollectorResult> mappedStream =
                 env.addSource(new FlinkKafkaConsumer09<>(INBOUND_TOPIC, new SimpleStringSchema(), properties))
-                        .flatMap((rawJsonInput, out) -> {
-                            final CollectorResult collectorResult = JsonUtils.readObject(CollectorResult.class, rawJsonInput);
-                            out.collect(collectorResult);
+                        .flatMap(new FlatMapFunction<String, CollectorResult>() {
+                            @Override
+                            public void flatMap(final String rawJsonInput, final Collector<CollectorResult> out) throws Exception {
+                                final CollectorResult collectorResult = JsonUtils.readObject(CollectorResult.class, rawJsonInput);
+                                out.collect(collectorResult);
+                            }
                         });
 
-        final DataStream<CollectorResult> collectorFlinkJmxStream = mappedStream.filter(collectorResult ->
-                collectorResult.getCollectorType().equals("flink_jmx"));
+        final DataStream<CollectorResult> collectorFlinkJmxStream = mappedStream.filter(new FilterFunction<CollectorResult>() {
+            @Override
+            public boolean filter(final CollectorResult collectorResult) throws Exception {
+                return collectorResult.getCollectorType().equals("flink_jmx");
+            }
+        });
 
-        final DataStream<String> resultStream = collectorFlinkJmxStream.map(flinkJmxResult -> {
-            final Map<String, Object> dataMap = new LinkedHashMap<>();
-            dataMap.put("client-timestamp", extractOrUnknown(flinkJmxResult.getClientTimestamp()));
-            dataMap.put("client-host", extractOrUnknown(flinkJmxResult.getClientHost()));
-            dataMap.put("client-port", extractOrUnknown(flinkJmxResult.getClientPort()));
-            dataMap.put("client-instance-id", extractOrUnknown(flinkJmxResult.getInstanceId()));
-            final Map<String, Object> jvmMap = Optional.ofNullable(flinkJmxResult.getData())
-                    .map(CollectorDataProcessor::extractJvmData)
-                    .orElse(new LinkedHashMap<>());
-            dataMap.putAll(jvmMap);
-            return JsonUtils.toJson(dataMap);
+        final DataStream<String> resultStream = collectorFlinkJmxStream.map(new MapFunction<CollectorResult, String>() {
+            @Override
+            public String map(final CollectorResult collectorResult) throws Exception {
+                final Map<String, Object> dataMap = new LinkedHashMap<>();
+                dataMap.put("client-timestamp", extractOrUnknown(collectorResult.getClientTimestamp()));
+                dataMap.put("client-host", extractOrUnknown(collectorResult.getClientHost()));
+                dataMap.put("client-port", extractOrUnknown(collectorResult.getClientPort()));
+                dataMap.put("client-instance-id", extractOrUnknown(collectorResult.getInstanceId()));
+                Map<String, Object> jvmMap = new LinkedHashMap<>();
+                if (collectorResult.getData() != null) {
+                    jvmMap = extractJvmData(collectorResult.getData());
+                }
+                dataMap.putAll(jvmMap);
+                return JsonUtils.toJson(dataMap);
+            }
         });
         resultStream.addSink(new FlinkKafkaProducer09<>(KAFKA_HOST, OUTBOUND_TOPIC, new SimpleStringSchema()));
         env.execute();
+
+//        TODO: THIS WORKED GREAT, BUT SUDDENLY NOT ANYMORE, SO SWITCHED TO 1.7 FOR PROTOTYPE
+//
+//        final DataStream<CollectorResult> mappedStream =
+//                env.addSource(new FlinkKafkaConsumer09<>(INBOUND_TOPIC, new SimpleStringSchema(), properties))
+//                        .flatMap((rawJsonInput, out) -> {
+//                            final CollectorResult collectorResult = JsonUtils.readObject(CollectorResult.class, rawJsonInput);
+//                            out.collect(collectorResult);
+//                        });
+//
+//        final DataStream<CollectorResult> collectorFlinkJmxStream = mappedStream.filter(collectorResult ->
+//                collectorResult.getCollectorType().equals("flink_jmx"));
+//
+//        final DataStream<String> resultStream = collectorFlinkJmxStream.map(flinkJmxResult -> {
+//            final Map<String, Object> dataMap = new LinkedHashMap<>();
+//            dataMap.put("client-timestamp", extractOrUnknown(flinkJmxResult.getClientTimestamp()));
+//            dataMap.put("client-host", extractOrUnknown(flinkJmxResult.getClientHost()));
+//            dataMap.put("client-port", extractOrUnknown(flinkJmxResult.getClientPort()));
+//            dataMap.put("client-instance-id", extractOrUnknown(flinkJmxResult.getInstanceId()));
+//            final Map<String, Object> jvmMap = Optional.ofNullable(flinkJmxResult.getData())
+//                    .map(CollectorDataProcessor::extractJvmData)
+//                    .orElse(new LinkedHashMap<>());
+//            dataMap.putAll(jvmMap);
+//            return JsonUtils.toJson(dataMap);
+//        });
+//        resultStream.addSink(new FlinkKafkaProducer09<>(KAFKA_HOST, OUTBOUND_TOPIC, new SimpleStringSchema()));
+//        env.execute();
     }
 
     private static String extractOrUnknown(final Object value) {
-        return Optional.ofNullable(value).map(Object::toString).orElse("unknown");
+        return (value == null) ? "unknown" : value.toString();
+//        return Optional.ofNullable(value).map(Object::toString).orElse("unknown");
     }
 
     private static Map<String, Object> extractJvmData(final Map<String, Object> data) {
-        final Object jobmanager = Optional.ofNullable(data.get("jobmanager"))
-                .orElseThrow(() -> new CollectorDataProcessorException("Invalid data, 'jobmanager' element required"));
+        if (data.get("jobmanager") == null) {
+            throw new CollectorDataProcessorException("Invalid data, 'jobmanager' element required");
+        }
+        final Object jobmanager = data.get("jobmanager");
         final Map<String, Object> jvmResultMap = new LinkedHashMap<>();
         if (jobmanager instanceof List) {
             final List<Map<String, Object>> jm = (List<Map<String, Object>>) jobmanager;
