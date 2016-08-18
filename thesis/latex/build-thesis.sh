c871f27713f78444f142a424e4283da969db1803
Markus Lamm <markus.lamm@googlemail.com>
2016-08-18 18:56:16 +0200

    Working references

---
+++ thesis/latex/build-thesis.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+rm main.acn main.aux main.bcf main.glo main.idx main.ilg main.ind main.ist main.lof main.log main.lol main.lot main.out main.run.xml main.synctex.gz main.toc
+
+lualatex -synctex=1 -interaction=nonstopmode main && \
+biber main && \
+makeindex main  && \
+makeindex -s main.ist -t main.alg -o main.acr main.acn  && \
+makeindex -s main.ist -t main.glg -o main.gls main.glo  && \
+lualatex -synctex=1 -interaction=nonstopmode main  && \
+lualatex -synctex=1 -interaction=nonstopmode main
